08b529f806f1bfd8fee68cee5495a8576e82e683
Kevin Tumbo <kevin.tumbo39@gmail.com>
2018-04-13 13:58:10 +0300

    set JWT_Secret env

--- server/models/users.js
+++ server/models/users.js
@@ -82,7 +82,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
     const user = this;
     const access = 'auth';
-    const token = jwt.sign({_id: user._id.toHexString(), access}, '1234qwer!"£$%&').toString();
+    const token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
     user.tokens = user.tokens.concat([{access, token}]);
 
     const currentDate = new Date();
@@ -109,7 +109,7 @@ UserSchema.statics.findByToken = function (token) {
     let decoded;
 
     try {
-        decoded = jwt.verify(token, '1234qwer!"£$%&');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (e) {
         return Promise.reject(e);
     }
